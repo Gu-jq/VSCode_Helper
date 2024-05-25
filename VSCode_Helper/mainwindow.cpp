6a0b1989aafdd7adce65544bd240d33478f8af63
Gu-jq <151929480+Gu-jq@users.noreply.github.com>
2024-05-26 01:55:05 +0800

    Add files via upload

--- VSCode_Helper/mainwindow.cpp
+++ VSCode_Helper/mainwindow.cpp
@@ -22,40 +22,92 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    QUrl url("https://marketplace.visualstudio.com/items?itemName=MS-CEINTL.vscode-language-pack-zh-hans");
+    QByteArray username = qgetenv("USERNAME");
+    path = QString(username).toStdString();
+    path = "C:\\users\\" + path + "\\AppData\\Local\\Programs\\Microsoft VS Code\\";
+    if (QFile::exists((path + "Code.exe").c_str())) {
+        // 找到
+        ui->stackedWidget->setCurrentIndex(2);
+        return;
+    } else {
+        // 未找到，尝试在D盘寻找
+        path = "D:\\Microsoft VS Code\\"; //测试阶段，这里改成X盘使得VSCode无法直接被找到，记得改回去
+        if (QFile::exists((path + "Code.exe").c_str())) {
+            // 找到
+            ui->stackedWidget->setCurrentIndex(2);
+            return;
+        }
+        else{
+            ui->stackedWidget->setCurrentIndex(1);
+            ui->notice_1->setText(QString(" "));
+            ui->cnext_button_1->hide();
+            return;
+        }
+    }
+
+}
+
+void MainWindow::on_pushButton_9_clicked()
+{
+    QUrl url("https://code.visualstudio.com/Download");
     if (!QDesktopServices::openUrl(url)) {
         return;
     }
 }
 
 
-void MainWindow::on_pushButton_2_clicked()
+void MainWindow::on_pushButton_12_clicked()
 {
-    ui->stackedWidget->setCurrentIndex(1);
+    ui->stackedWidget->setCurrentIndex(0);
 }
 
 
-void MainWindow::on_pushButton_3_clicked()
+void MainWindow::on_pushButton_10_clicked()
 {
     QByteArray username = qgetenv("USERNAME");
     path = QString(username).toStdString();
     path = "C:\\users\\" + path + "\\AppData\\Local\\Programs\\Microsoft VS Code\\";
-    qDebug() << path;
     if (QFile::exists((path + "Code.exe").c_str())) {
-        // 找到应用
-        return;
+        // 找到
+        ui->notice_1->setText(QString("成功找到VS Code"));
+        ui->cnext_button_1->show();
     } else {
         // 未找到，尝试在D盘寻找
-        path = "D:\\Microsoft VS Code\\";
-        qDebug() << path;
+        path = "D:\\Microsoft VS Code\\"; //测试阶段，这里改成X盘使得VSCode无法直接被找到，记得改回去
         if (QFile::exists((path + "Code.exe").c_str())) {
-            // 找到应用
-            return;
+            // 找到
+            ui->notice_1->setText(QString("成功找到VS Code"));
+            ui->cnext_button_1->show();
         }
         else{
-            qDebug() << "open code failed";
-            return;
+            path = ui->path_input->text().toStdString();
+            qDebug() << path;
+            if (QFile::exists((path + "Code.exe").c_str())) {
+                // 找到
+                ui->notice_1->setText(QString("成功找到VS Code"));
+                ui->cnext_button_1->show();
+            }
+            else if(QFile::exists((path + "\\Code.exe").c_str())){
+                // 找到
+                ui->notice_1->setText(QString("成功找到VS Code"));
+                ui->cnext_button_1->show();
+            }
+            else{
+                ui->notice_1->setText(QString("未找到，请检查是否安装及输入路径正确性"));
+            }
         }
     }
+}
+
+
+void MainWindow::on_cnext_button_1_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+
+void MainWindow::on_pushButton_14_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
 }
 
--- VSCode_Helper/mainwindow.h
+++ VSCode_Helper/mainwindow.h
@@ -22,13 +22,20 @@ public:
 private slots:
     void on_pushButton_clicked();
 
-    void on_pushButton_2_clicked();
+    void on_pushButton_9_clicked();
 
-    void on_pushButton_3_clicked();
+    void on_pushButton_12_clicked();
+
+    void on_pushButton_10_clicked();
+
+    void on_cnext_button_1_clicked();
+
+    void on_pushButton_14_clicked();
 
 private:
     Ui::MainWindow *ui;
     QProcess *process_code;
     std::string path;
+    bool find_vsc;
 };
 #endif // MAINWINDOW_H
