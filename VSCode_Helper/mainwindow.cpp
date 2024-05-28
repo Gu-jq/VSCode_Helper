2fe08a7514a2150e52bdc22fa37b958e57e44313
Gu-jq <151929480+Gu-jq@users.noreply.github.com>
2024-05-28 22:42:25 +0800

    Add files via upload

--- VSCode_Helper/mainwindow.cpp
+++ VSCode_Helper/mainwindow.cpp
@@ -31,7 +31,7 @@ void MainWindow::on_pushButton_clicked()
         return;
     } else {
         // 未找到，尝试在D盘寻找
-        path = "D:\\Microsoft VS Code\\"; //测试阶段，这里改成X盘使得VSCode无法直接被找到，记得改回去
+        path = "X:\\Microsoft VS Code\\"; //测试阶段，这里改成X盘使得VSCode无法直接被找到，记得改回去
         if (QFile::exists((path + "Code.exe").c_str())) {
             // 找到
             ui->stackedWidget->setCurrentIndex(2);
@@ -73,7 +73,7 @@ void MainWindow::on_pushButton_10_clicked()
         ui->cnext_button_1->show();
     } else {
         // 未找到，尝试在D盘寻找
-        path = "D:\\Microsoft VS Code\\"; //测试阶段，这里改成X盘使得VSCode无法直接被找到，记得改回去
+        path = "X:\\Microsoft VS Code\\"; //测试阶段，这里改成X盘使得VSCode无法直接被找到，记得改回去
         if (QFile::exists((path + "Code.exe").c_str())) {
             // 找到
             ui->notice_1->setText(QString("成功找到VS Code"));
