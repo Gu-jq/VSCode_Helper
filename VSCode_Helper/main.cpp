6babd067f39fbfdff79cdc9ccaa142a8a10fefcf
Gu-jq <151929480+Gu-jq@users.noreply.github.com>
2024-06-29 23:29:56 +0800

    Add files via upload

--- VSCode_Helper/main.cpp
+++ VSCode_Helper/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setStyleSheet("* { color: rgb(0, 0, 0); }");
     MainWindow w;
-    w.setFixedSize(QSize(480, 480));
+    w.setFixedSize(QSize(800, 480));
     w.setWindowTitle("VS Code 小帮手");
     w.show();
     return a.exec();
